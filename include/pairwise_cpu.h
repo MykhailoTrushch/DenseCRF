4def9f3d6eaebbcc6bd7a67ed1ba8a2ad59f6d72
MykhailoTrushch <mykhailo.trushch@tum.de>
2025-04-22 12:17:25 +0200

    added symmetric normalization to GPU implementation

--- include/pairwise_cpu.h
+++ include/pairwise_cpu.h
@@ -6,108 +6,108 @@
 namespace dcrf_cuda
 {
 
-    template <int M, int F>
-    class PottsPotentialCPU : public PairwisePotential
+template <int M, int F>
+class PottsPotentialCPU : public PairwisePotential
+{
+protected:
+    PermutohedralLatticeCPU lattice_;
+    float w_;
+    float *norm_;
+    NormalizationType ntype_;
+
+public:
+    PottsPotentialCPU(const float *features, int N, float w, NormalizationType ntype = NORMALIZE_SYMMETRIC) : PairwisePotential(N), w_(w), ntype_(ntype)
     {
-    protected:
-        PermutohedralLatticeCPU lattice_;
-        float w_;
-        float *norm_;
-        NormalizationType ntype_;
-
-    public:
-        PottsPotentialCPU(const float *features, int N, float w, NormalizationType ntype = NORMALIZE_SYMMETRIC) : PairwisePotential(N), w_(w), ntype_(ntype)
+        lattice_.init(features, F, N);
+        norm_ = new float[N];
+        std::fill(norm_, norm_ + N, 1.0f);
+        lattice_.compute(norm_, norm_, 1);
+        if (ntype == NO_NORMALIZATION)
         {
-            lattice_.init(features, F, N);
-            norm_ = new float[N];
-            std::fill(norm_, norm_ + N, 1.0f);
-            lattice_.compute(norm_, norm_, 1);
-            if (ntype == NO_NORMALIZATION)
+            float mean_norm = 0;
+            for (int i = 0; i < N; i++)
+                mean_norm += norm_[i];
+            mean_norm = N / mean_norm;
+            for (int i = 0; i < N; i++)
+                norm_[i] = mean_norm;
+        }
+        else if (ntype == NORMALIZE_SYMMETRIC)
+        {
+            for (int i = 0; i < N_; ++i)
             {
-                float mean_norm = 0;
-                for (int i = 0; i < N; i++)
-                    mean_norm += norm_[i];
-                mean_norm = N / mean_norm;
-                for (int i = 0; i < N; i++)
-                    norm_[i] = mean_norm;
+                norm_[i] = 1.0f / sqrt(norm_[i] + 1e-20f);
             }
-            else if (ntype == NORMALIZE_SYMMETRIC)
+        }
+        else
+        {
+            for (int i = 0; i < N_; ++i)
             {
-                for (int i = 0; i < N_; ++i)
-                {
-                    norm_[i] = 1.0f / sqrt(norm_[i] + 1e-20f);
-                }
+                norm_[i] = 1.0f / sqrt(norm_[i] + 1e-20f);
             }
-            else
+        }
+    }
+
+    ~PottsPotentialCPU()
+    {
+        delete[] norm_;
+    }
+
+    PottsPotentialCPU(const PottsPotentialCPU &o) = delete;
+
+    //// Factory functions:
+    // Build image-based potential: if features is NULL then applying gaussian filter only.
+    template <class T = float>
+    static PottsPotentialCPU<M, F> *FromImage(int w, int h, float weight, float posdev, const T *features = nullptr, float featuredev = 0.0)
+    {
+        // First assemble features:
+        auto *allFeatures = new float[F * w * h];
+        for (int hi = 0; hi < h; ++hi)
+        {
+            for (int wi = 0; wi < w; ++wi)
             {
-                for (int i = 0; i < N_; ++i)
+                const int idx = hi * w + wi;
+                allFeatures[idx * F + 0] = (float)wi / posdev;
+                allFeatures[idx * F + 1] = (float)hi / posdev;
+                for (int i = 2; i < F; ++i)
                 {
-                    norm_[i] = 1.0f / sqrt(norm_[i] + 1e-20f);
+                    allFeatures[idx * F + i] = (float)features[idx * (F - 2) + (i - 2)] / featuredev;
                 }
             }
         }
+        auto *pt = new PottsPotentialCPU<M, F>(allFeatures, w * h, weight);
+        delete[] allFeatures;
+        return pt;
+    }
 
-        ~PottsPotentialCPU()
+    void apply(float *out_values, const float *in_values, float *tmp) const
+    {
+        if (ntype_ == NORMALIZE_SYMMETRIC || ntype_ == NORMALIZE_BEFORE)
         {
-            delete[] norm_;
-        }
-
-        PottsPotentialCPU(const PottsPotentialCPU &o) = delete;
-
-        //// Factory functions:
-        // Build image-based potential: if features is NULL then applying gaussian filter only.
-        template <class T = float>
-        static PottsPotentialCPU<M, F> *FromImage(int w, int h, float weight, float posdev, const T *features = nullptr, float featuredev = 0.0)
-        {
-            // First assemble features:
-            auto *allFeatures = new float[F * w * h];
-            for (int hi = 0; hi < h; ++hi)
-            {
-                for (int wi = 0; wi < w; ++wi)
-                {
-                    const int idx = hi * w + wi;
-                    allFeatures[idx * F + 0] = (float)wi / posdev;
-                    allFeatures[idx * F + 1] = (float)hi / posdev;
-                    for (int i = 2; i < F; ++i)
-                    {
-                        allFeatures[idx * F + i] = (float)features[idx * (F - 2) + (i - 2)] / featuredev;
-                    }
-                }
-            }
-            auto *pt = new PottsPotentialCPU<M, F>(allFeatures, w * h, weight);
-            delete[] allFeatures;
-            return pt;
-        }
-
-        void apply(float *out_values, const float *in_values, float *tmp) const
-        {
-            if (ntype_ == NORMALIZE_SYMMETRIC || ntype_ == NORMALIZE_BEFORE)
-            {
-                for (int i = 0, k = 0; i < N_; i++)
-                    for (int j = 0; j < M; j++, k++)
-                        tmp[k] = norm_[i] * in_values[k];
-            }
-            else
-            {
-                std::memcpy(tmp, in_values, N_ * M);
-            }
-            lattice_.compute(tmp, tmp, M);
             for (int i = 0, k = 0; i < N_; i++)
-            {
                 for (int j = 0; j < M; j++, k++)
+                    tmp[k] = norm_[i] * in_values[k];
+        }
+        else
+        {
+            std::memcpy(tmp, in_values, N_ * M);
+        }
+        lattice_.compute(tmp, tmp, M);
+        for (int i = 0, k = 0; i < N_; i++)
+        {
+            for (int j = 0; j < M; j++, k++)
+            {
+                // multiplying by w_ is applying the Potts label compatibility
+                if (ntype_ == NORMALIZE_SYMMETRIC || ntype_ == NORMALIZE_AFTER)
                 {
-                    // multiplying by w_ is applying the Potts label compatibility
-                    if (ntype_ == NORMALIZE_SYMMETRIC || ntype_ == NORMALIZE_AFTER)
-                    {
-                        out_values[k] += w_ * norm_[i] * tmp[k];
-                    }
-                    else
-                    {
-                        out_values[k] += w_ * tmp[k];
-                    }
+                    out_values[k] += w_ * norm_[i] * tmp[k];
+                }
+                else
+                {
+                    out_values[k] += w_ * tmp[k];
                 }
             }
         }
-    };
+    }
+};
 
 }
--- include/pairwise_gpu.cuh
+++ include/pairwise_gpu.cuh
@@ -46,25 +46,76 @@ __global__ static void assembleUnorganizedFeature(int N, int pdim, const PT* pos
     }
 }
 
+// Normalizes the data used in kernels.
+template<int M, int F>
+__global__ static void normalize(float* out, const float* in, const float* norm, const int n, const float pw) {
+    const int ni = threadIdx.x + blockIdx.x * blockDim.x;
+    const int vi = blockIdx.y;
+    if (ni >= n) return;
+    out[ni * M + vi] += pw * norm[ni] * in[ni * M + vi];
+}
+
+__global__ void fillOnes(float* norm, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= N) return;
+    norm[i] = 1.0f;
+}
+
+// __global__ void setNormVal(float* norm, int N, NormalizationType ntype) {
+//     int i = blockIdx.x * blockDim.x + threadIdx.x;
+//     if (i >= N) return;
+//     if (ntype == NORMALIZE_SYMMETRIC) {
+//         norm[i] = 1.0f / sqrtf(norm[i] + 1e-20f);
+//     } else if (ntype == NORMALIZE_AFTER || ntype == NORMALIZE_BEFORE) {
+//         norm[i] = 1.0f / (norm[i] + 1e-20f);
+//     }
+// }
+
+__global__ void inverseSqrtKernel(float* data, int N) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N) {
+        data[i] = 1.0f / sqrtf(data[i] + 1e-20f);
+    }
+}
+
 template<int M, int F>
 class PottsPotentialGPU: public PairwisePotential {
 protected:
     PermutohedralLatticeGPU<float, F, M + 1>* lattice_;
     float w_;
+    NormalizationType ntype_;
+    float *norm_;
 public:
-    PottsPotentialGPU(const float* features, int N, float w) : PairwisePotential(N), w_(w) {
+    PottsPotentialGPU(const float* features, int N, float w, NormalizationType ntype = NORMALIZE_SYMMETRIC) : PairwisePotential(N), w_(w), ntype_(ntype) {
         lattice_ = new PermutohedralLatticeGPU<float, F, M + 1>(N);
         lattice_->prepare(features);
+
+        PermutohedralLatticeGPU<float, F, 2>* latticeNorm = new PermutohedralLatticeGPU<float, F, 2>(N);
+        latticeNorm->prepare(features);
+
+        dim3 blocks((N_ - 1) / BLOCK_SIZE + 1, M, 1);
+        dim3 blockSize(BLOCK_SIZE, 1, 1);
+
+        float* ones_input;
+        cudaMalloc(&ones_input, N * sizeof(float));
+        fillOnes<<<(N + 255) / 256, 256>>>(ones_input, N);
+
+        cudaMalloc(&norm_, N * sizeof(float));
+        cudaMemset(norm_, 0, N * sizeof(float));
+
+        latticeNorm->filter(norm_, ones_input);
+        inverseSqrtKernel<<<(N + 255) / 256, 256>>>(norm_, N);
     }
     ~PottsPotentialGPU(){
         delete lattice_;
+        cudaFree(norm_);
     }
     PottsPotentialGPU( const PottsPotentialGPU&o ) = delete;
 
-    //// Factory functions:
+    // Factory functions:
     // Build image-based potential: if features is NULL then applying gaussian filter only.
     template<class T = float>
-    static PottsPotentialGPU<M, F>* FromImage(int w, int h, float weight, float posdev, const T* features = nullptr, float featuredev = 0.0) {
+    static PottsPotentialGPU<M, F>* FromImage(int w, int h, float weight, float posdev, const T* features = nullptr, float featuredev = 0.0, NormalizationType ntype = NORMALIZE_SYMMETRIC) {
         // First assemble features:
         float* allFeatures = nullptr;
         cudaMalloc((void**)&allFeatures, sizeof(float) * F * w * h);
@@ -72,7 +123,7 @@ public:
         dim3 blockSize(16, 16, 1);
         assembleImageFeature<T, M, F> <<<blocks, blockSize>>> (w, h, features, posdev, featuredev, allFeatures);
         cudaErrorCheck();
-        auto* pt = new PottsPotentialGPU<M, F>(allFeatures, w * h, weight);
+        auto* pt = new PottsPotentialGPU<M, F>(allFeatures, w * h, weight, ntype);
         cudaFree(allFeatures);
         return pt;
     }
@@ -94,10 +145,16 @@ public:
     // tmp should be larger to store normalization values. (N*(M+1))
     // All pointers are device pointers
     void apply(float* out_values, const float* in_values, float* tmp) const {
-        lattice_->filter(tmp, in_values);
-        dim3 blocks((N_ - 1) / BLOCK_SIZE + 1, M, 1);
-        dim3 blockSize(BLOCK_SIZE, 1, 1);
-        pottsWeight<M, F> <<<blocks, blockSize>>> (out_values, tmp, N_, w_);
+        // N_ = W * H;
+        // blocks = ((W * H - 1) / 256 + 1, M, 1);
+        // blocksize is (256, 1, 1)
+        dim3 blocks((N_ - 1) / BLOCK_SIZE + 1, M);
+        dim3 blockSize(BLOCK_SIZE);
+        normalize<M, F> <<<blocks, blockSize>>> (tmp, in_values, norm_, N_, w_);
+        float *out_tmp;
+        cudaMalloc(&out_tmp, N_ * M * sizeof(float));
+        lattice_->filter(out_tmp, tmp);
+        pottsWeight<M, F> <<<blocks, blockSize>>> (out_values, out_tmp, N_, w_);
     }
 };
 
